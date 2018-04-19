f63579fdd3f4f6ba708448793761e4781382bd22
jhpark2018 <jinsal69@naver.com>
2018-04-19 15:51:36 +0900

    speed1->speed

--- p0103/src/p0103/Ex00.java
+++ p0103/src/p0103/Ex00.java
@@ -2,12 +2,12 @@ package p0103;
 
 class Car{
 	String color;
-	int speed1;
+	int speed;
 	void upSpeed(int value) {
-		this.speed1=this.speed1 + value;
+		this.speed=this.speed + value;
 	}
 	void downSpeed(int value) {
-		speed1 = speed1 - value;
+		speed = speed - value;
 	}
 }
 public class Ex00 {
@@ -15,35 +15,35 @@ public class Ex00 {
 	public static void main(String[] args) {
 		Car myCar1 = new Car();
 		myCar1.color = "빨강";
-		myCar1.speed1 = 0;
+		myCar1.speed = 0;
 		
 		Car myCar2 = new Car();
 		myCar2.color = "파랑";
-		myCar2.speed1 = 0;
+		myCar2.speed = 0;
 		
 		Car myCar3 = new Car();
 		myCar3.color = "노랑";
-		myCar3.speed1 = 0;
+		myCar3.speed = 0;
 		
 		System.out.println("자동차1의 색상은 "+myCar1.color+ " 속도는 "
-		     +myCar1.speed1+"km 입니다.");
+		     +myCar1.speed+"km 입니다.");
 		System.out.println("자동차2의 색상은 "+myCar2.color+ " 속도는 "
-			     +myCar2.speed1+"km 입니다.");
+			     +myCar2.speed+"km 입니다.");
 		System.out.println("자동차3의 색상은 "+myCar3.color+ " 속도는 "
-			     +myCar3.speed1+"km 입니다.");
+			     +myCar3.speed+"km 입니다.");
 		myCar1.upSpeed(60);
 		myCar2.upSpeed(80);
 		myCar3.upSpeed(100);
 		System.out.println("자동차1의 색상은 "+myCar1.color+ " 속도는 "
-			     +myCar1.speed1+"km 입니다.");
+			     +myCar1.speed+"km 입니다.");
 		System.out.println("자동차2의 색상은 "+myCar2.color+ " 속도는 "
-				 +myCar2.speed1+"km 입니다.");
+				 +myCar2.speed+"km 입니다.");
 		System.out.println("자동차3의 색상은 "+myCar3.color+ " 속도는 "
-				 +myCar3.speed1+"km 입니다.");
+				 +myCar3.speed+"km 입니다.");
 		
-		myCar1.speed1 =120;
+		myCar1.speed =120;
 		System.out.println("자동차1의 색상은 "+myCar1.color+ " 속도는 "
-			     +myCar1.speed1+"km 입니다.");
+			     +myCar1.speed+"km 입니다.");
 	}
 
 }
